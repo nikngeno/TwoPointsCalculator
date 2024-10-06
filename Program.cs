c1e017c21d46676d379c97c8c43e8e0bc94f8822
Nick <nikngeno@gmail.com>
2024-10-06 14:24:49 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TwoPointsCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Distance(7, 4, 3, 17.0, 6.0, 2.0));
+        }
+        public static double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            // your code is here
+            double d = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+            return d;
+        }
+    }
+}
